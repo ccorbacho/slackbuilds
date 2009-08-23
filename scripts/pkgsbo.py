799811b977984484b60c104c256addfce56912d6
Carlos Corbacho <carlos@strangeworlds.co.uk>
2009-08-23 14:23:12 +0100

    Add script for generating SBo tarballs
    Add a helper script for converting a directory into an SBo compliant
    tarball.

---
+++ scripts/pkgsbo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""\
+%prog PACKAGE
+
+Convert a package source directory into a tarball ready for submission to
+SlackBuilds.org
+"""
+
+import os
+import subprocess
+import sys
+import tarfile
+import tempfile
+
+
+def main(args):
+    if len(args) <= 0:
+        sys.exit("Invalid number of arguments")
+    package_name = args[0]
+    tempdir = tempfile.mkdtemp()
+    subprocess.call(["rsync",
+                     "--recursive",
+                     "--cvs-exclude",
+                     "--exclude", ".svn",
+                     "--exclude", ".git",
+                     "--exclude", "*.tar.*",
+                     package_name, tempdir])
+    subprocess.call(["tar",
+                     "--create",
+                     "--file", "%s.tar" % package_name,
+                     "--directory", tempdir, "."])
+    subprocess.call(["gzip", "%s.tar" % package_name])
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
